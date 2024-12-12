cc801ff3d2555f254829e1ed4015419c50409d81
Faria <fariananin81@gmail.com>
2024-12-13 01:44:32 +0600

    Task 3 & 4

--- index.js
+++ index.js
@@ -26,3 +26,32 @@ function ODD(x){
 }
 
 console.log(ODD(5))
+
+// Write a function named findMin that takes an array of numbers 
+//and returns the smallest number from the array.
+
+function findMin(numarray){
+    let result=Math.min(...numarray);//passing every Value of the array
+    return result
+}
+var numarray= [4,5,6,7,8,9,2,10];
+console.log(findMin(numarray));
+
+
+//Task 4
+
+function filterEvenNumbers(arr){
+    var i;
+    const newarr=[];
+    for(i=0;i<arr.length;i++){
+        if(arr[i]%2===0){
+         newarr.push(arr[i]);
+        }
+
+    }
+    return newarr;
+
+}
+
+let arr= [2,3,6,7,8,20,16];
+console.log(filterEvenNumbers(arr));
